822e8f4b19f32fefd9996641484610f26a16dc9b
Alesha Neeamuth <alyshanee@gmail.com>
2019-03-30 17:21:28 +0000

    add crime type dimension, group and create row chart

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,5 +1,38 @@
 queue()
     .defer(d3.csv, "data/2018-btp.csv")
     .await(makeGraphs);
-    
-    function (makeGraphs)
+
+
+function makeGraphs(error, crimeData) {
+
+    crimeData.forEach(function(d) {
+
+    })
+
+    var ndx = crossfilter(crimeData);
+
+    show_crimes_by_type(ndx);
+    dc.renderAll();
+
+
+};
+
+function show_crimes_by_type(ndx) {
+
+
+    var typeDimension = ndx.dimension(dc.pluck("Crime type"));
+    var typeGroup = typeDimension.group();
+
+    var rowChart = dc.rowChart("#total-crime-by-type")
+        .height(500)
+        .width(1300)
+        .dimension(typeDimension)
+        .group(typeGroup);
+
+
+
+
+
+
+
+}
