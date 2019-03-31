847f62b47f82fff45ec3d36ee1cf4d51914cafbe
Alesha Neeamuth <alyshanee@gmail.com>
2019-03-31 10:23:28 +0000

    convert string date into dates, create date dimension, group and line chart

--- static/js/graph.js
+++ static/js/graph.js
@@ -3,15 +3,20 @@ queue()
     .await(makeGraphs);
 
 
-function makeGraphs(error, crimeData) {
+function makeGraphs(error, data) {
 
-    crimeData.forEach(function(d) {
+    data.forEach(function(d) {
+
+        var tempDate = new Date(d.Month);
+        d.Month = tempDate;
 
     })
 
-    var ndx = crossfilter(crimeData);
 
+    var ndx = crossfilter(data);
+    show_crimes_by_month(ndx);
     show_crimes_by_type(ndx);
+
     dc.renderAll();
 
 
@@ -19,20 +24,39 @@ function makeGraphs(error, crimeData) {
 
 function show_crimes_by_type(ndx) {
 
-
     var typeDimension = ndx.dimension(dc.pluck("Crime type"));
     var typeGroup = typeDimension.group();
 
     var rowChart = dc.rowChart("#total-crime-by-type")
         .height(500)
-        .width(1300)
+        .width(1360)
         .dimension(typeDimension)
         .group(typeGroup);
 
 
+};
+
+function show_crimes_by_month(ndx) {
+
+    var dateDimension = ndx.dimension(dc.pluck("Month"));
+    var dateGroup = dateDimension.group();
+
+
+    var maxDate = dateDimension.top(1)[0].Month;
+    var minDate = dateDimension.bottom(1)[0].Month;
+
+    var lineChart = dc.lineChart("#total-crime-by-month")
+        .height(400)
+        .width(1360)
+        .dimension(dateDimension)
+        .group(dateGroup)
+        .renderHorizontalGridLines(true)
+        .x(d3.time.scale().domain([minDate, maxDate]));
+    lineChart.yAxis().ticks(5)
 
 
 
 
 
-}
+
+};
