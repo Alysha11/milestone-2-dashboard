85d52f1f48a1c27743a9fb3caa4ae587e3a494e6
Alesha Neeamuth <alyshanee@gmail.com>
2019-04-04 21:49:10 +0000

    add by station chart , add by station selector, tidy csv and re-add

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,40 +1,45 @@
+// Promise to load data, once comple makeGraphs is executed. 
 queue()
     .defer(d3.csv, "data/2018-btp.csv")
     .await(makeGraphs);
 
-
+//function to render graphs
 function makeGraphs(error, data) {
+    
 
+
+    //convert string dates to js dates in data
     data.forEach(function(d) {
 
         var tempDate = new Date(d.Month);
         d.Month = tempDate;
 
-    })
+    });
 
 
     var ndx = crossfilter(data);
+    show_station_selector(ndx)
     show_crimes_by_month(ndx);
     show_crimes_by_type(ndx);
+    show_crimes_by_station(ndx);
+    show_north_vs_south(ndx);
 
     dc.renderAll();
 
 
 };
 
-function show_crimes_by_type(ndx) {
+function show_station_selector(ndx) {
 
-    var typeDimension = ndx.dimension(dc.pluck("Crime type"));
-    var typeGroup = typeDimension.group();
+        var stationDimension = ndx.dimension(dc.pluck("Location"));
+        var stationGroup = stationDimension.group();
+        dc.selectMenu("#station_selector")
+                .dimension(stationDimension)
+                .group(stationGroup)
+                .promptText('Select Station');
 
-    var rowChart = dc.rowChart("#total-crime-by-type")
-        .height(500)
-        .width(1360)
-        .dimension(typeDimension)
-        .group(typeGroup);
+}
 
-
-};
 
 function show_crimes_by_month(ndx) {
 
@@ -56,7 +61,61 @@ function show_crimes_by_month(ndx) {
 
 
 
+};
 
+
+function show_crimes_by_type(ndx) {
+
+    var typeDimension = ndx.dimension(dc.pluck("Crime"));
+    var typeGroup = typeDimension.group();
+
+    var rowChart = dc.rowChart("#total-crime-by-type")
+        .height(500)
+        .width(1360)
+        .dimension(typeDimension)
+        .group(typeGroup);
+
+
+};
+
+function show_crimes_by_station(ndx) {
+
+    var stationDimension = ndx.dimension(dc.pluck("Location"));
+    var stationGroup = stationDimension.group();
+
+
+    var rowChart = dc.rowChart("#total-crime-by-station")
+        .height(500)
+        .width(1360)
+        .dimension(stationDimension)
+        .group(stationGroup);
+
+
+};
+
+
+
+function show_north_vs_south(ndx) {
+    var northSouthDimension = ndx.dimension(function(d) {
+        if (d.Location === "Waterloo" || "London Bridge") {
+            return "South";
+       } else  {
+            return "North";
+            
+       }
+    });
+    
+    
+    var northSouthGroup = northSouthDimension.group();
+
+    
+    
+    dc.pieChart("#show_north_vs_south")
+    .height(300)
+    .width(90)
+    .dimension(northSouthDimension)
+    .group(northSouthGroup);
+    
 
 
 };
