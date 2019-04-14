0041b3d348dbcaecd0bd1d4a0786a581862f8693
Alesha Neeamuth <alyshanee@gmail.com>
2019-04-14 21:48:07 +0000

    add composite chart, format legends

--- static/js/graph.js
+++ static/js/graph.js
@@ -6,8 +6,6 @@ queue()
 //function to render graphs
 function makeGraphs(error, data) {
 
-
-
     //Convert string dates to js dates in data
     data.forEach(function(d) {
 
@@ -15,7 +13,6 @@ function makeGraphs(error, data) {
         d.Month = tempDate;
 
     });
-
 
     //Convert string to interger in data
 
@@ -29,13 +26,15 @@ function makeGraphs(error, data) {
     show_crimes_by_month(ndx);
     show_crimes_by_type(ndx);
     show_crimes_by_station(ndx);
-    show_north_vs_south(ndx);
-    //show_stacked_chart(ndx)
-    show_theft_distribution(ndx)
+    //show_north_vs_south(ndx);
+    show_theft_distribution(ndx);
+    showCrimeTimeStation(ndx);
     dc.renderAll();
 
 
 };
+
+//station selector
 
 function show_station_selector(ndx) {
 
@@ -48,7 +47,7 @@ function show_station_selector(ndx) {
 
 }
 
-
+//crimes by month line chart
 function show_crimes_by_month(ndx) {
 
     var dateDimension = ndx.dimension(dc.pluck("Month"));
@@ -95,22 +94,26 @@ function show_crimes_by_station(ndx) {
     var rowChart = dc.rowChart("#total-crime-by-station")
         .height(500)
         .width(1360)
-        .ordinalColors(['#3182bd', '#6baed6', '#9ecae1', '#c6dbef', '#dadaeb']) //play around with
+        .ordinalColors(['#3182bd', '#6baed6', '#9ecae1', '#c6dbef', '#dadaeb'])
         .dimension(stationDimension)
         .group(stationGroup);
 
 
 };
 
-function show_north_vs_south(ndx) {
+/*function show_north_vs_south(ndx) {
+
 
     var northSouthDimension = ndx.dimension(function(d) {
-        if (d.Location === "Waterloo" || d.Location === "London Bridge")
+        if (d.Location === ' Waterloo ')
             return "South";
-        else
-            return "North"
-
+        else if (d.Location === ' London  Bridge ')
+            return "South";
+        else if (d.Location === ' Euston ')
+            return "North";
     });
+
+
 
     var northSouthGroup = northSouthDimension.group();
 
@@ -118,10 +121,12 @@ function show_north_vs_south(ndx) {
         .height(180)
         .width(180)
         .dimension(northSouthDimension)
-        .group(northSouthGroup);
+        .group(northSouthGroup)
+        .label(function(d) {
+            return d.key + ': ' + d.value;
+        });
 
-};
-
+};*/
 
 function show_theft_distribution(ndx) {
 
@@ -180,16 +185,65 @@ function show_theft_distribution(ndx) {
         })
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
-        .legend(dc.legend().x(50).y(0).itemHeight(20).gap(20).horizontal(true).autoItemWidth(true).itemWidth(60))
-        .xAxisLabel("Station") 
-        .yAxisLabel("Total Incidents"); 
+        .legend(dc.legend().x(50).y(0).itemHeight(13).gap(5).horizontal(true).autoItemWidth(true).itemWidth(60))
+        .xAxisLabel("Station")
+        .yAxisLabel("Incidents")
+        .renderHorizontalGridLines(true);
 
     stackedChart.margins().right = 100;
 
+};
+
+
+function showCrimeTimeStation(ndx) {
+    var date_dim = ndx.dimension(dc.pluck('Month'));
+    var minDate = date_dim.bottom(1)[0].Month;
+    var maxDate = date_dim.top(1)[0].Month;
+
+    function spend_by_name(Location) {
+        return function(d) {
+            if (d.Location === Location) {
+                return +d.Counter;
+            }
+            else {
+                return 0;
+            }
+        }
+    }
+    var victoriaCrimesByMonth = date_dim.group().reduceSum(spend_by_name(' London Victoria '));
+    var waterlooCrimesByMonth = date_dim.group().reduceSum(spend_by_name('Waterloo'));
+    var eustonCrimesByMonth = date_dim.group().reduceSum(spend_by_name(' Euston '));
+    var liverpoolStreetCrimesByMonth = date_dim.group().reduceSum(spend_by_name(' Liverpool Street '));
+    var londonBridgeCrimesByMonth = date_dim.group().reduceSum(spend_by_name(' London Bridge '));
+
+    var compositeChart = dc.compositeChart('#crime-time-station');
+    compositeChart
+        .width(990)
+        .height(200)
+        .dimension(date_dim)
+        .x(d3.time.scale().domain([minDate, maxDate]))
+        .legend(dc.legend().x(50).y(0).itemHeight(13).gap(5).horizontal(true).autoItemWidth(true).itemWidth(60))
+        .yAxisLabel("Incidents")
+        .renderHorizontalGridLines(true)
+        .compose([
+            dc.lineChart(compositeChart)
+            .colors('green')
+            .group(victoriaCrimesByMonth, ' London Victoria '),
+            dc.lineChart(compositeChart)
+            .colors('red')
+            .group(waterlooCrimesByMonth, 'Waterloo'),
+            dc.lineChart(compositeChart)
+            .colors('blue')
+            .group(eustonCrimesByMonth, ' Euston '),
+            dc.lineChart(compositeChart)
+            .colors('orange')
+            .group(liverpoolStreetCrimesByMonth, ' Liverpool Street '),
+            dc.lineChart(compositeChart)
+            .colors('pink')
+            .group(londonBridgeCrimesByMonth, ' London Bridge ')
+        ])
+        .brushOn(false)
 
 
 
 }
-
-
-
